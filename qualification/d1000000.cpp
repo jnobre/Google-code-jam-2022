b80be81a2a0832d316ae54e9ca451bbcc8f0e82b
João Nobre <joaoanobre@gmail.com>
2022-05-06 21:17:11 +0200

    Add solution to d1000000

---
+++ qualification/d1000000.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cstdio>
+
+#define MAXN 1000000
+
+using namespace std;
+
+int s[MAXN];
+
+int main() {
+  int t; 
+  scanf("%d\n", &t);
+  
+  for(int tc = 1; tc <= t; tc++) {
+    int n; 
+    scanf("%d\n", &n);
+    for(int i = 0; i < n; i++)
+      scanf("%d\n", &s[i]);
+
+    sort(s, s + n);
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+      if(s[i] > cnt) cnt++;
+    }
+    printf("Case #%d: %d\n", tc, cnt);
+  }
+
+  return 0;
+}
